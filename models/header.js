9039bcf981351db28e74b1c9eed32c6aa5b4527e
TheDummi <raydecrom@hotmail.com>
2023-10-12 20:37:28 +0200

    header

---
+++ models/header.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const header = document.getElementsByTagName('header')?.item(0);
+
+header.classList.add('center-top');
+
+header.innerHTML = `
+    <div class="header horizontal">ee
+    
+    </div>
+<div class="header horizontal"">
+ee
+    </div>
+<div class="header horizontal"">
+ee
+    </div>
+`;
+{
+	/* <a href="#"><div class="header-link"  id="belleaqua"></div></a>
+    <a href="#"><div class="header-link"  id="belleaqua"></div></a> */
+}
